0bc39f302ad04850d697546970a73c3f5db47ae2
Ratul22012 <it22012@mbstu.ac.bd>
2025-05-09 20:09:32 +0600

    Update Abstraction.java

--- Abstraction.java
+++ Abstraction.java
@@ -1,141 +1,136 @@
-import java.io.PrintStream; // needed when you want to create or use a PrintStream object explicitly
-
-//Interface: Defines engine behaviour that all vehicles must implement for startEngine and endEngine method
+import java.io.PrintStream;
 
 interface Engine {
-    PrintStream o = System.out;
     void startEngine();
-    void endEngine();
+    void stopEngine();
 }
 
-//abstract class: Serves as a base class for all vehicles
 abstract class Vehicle {
-    String brand;
+    String info;
+    static PrintStream o = System.out;
+
+    public Vehicle(String info) {
+        this.info = info;
+    }
+
+    abstract void drive();
+
+    public void displayInfo() {
+        o.println("Info: " + info);
+    }
+}
+
+class Car extends Vehicle implements Engine {
     int year;
-//Constructor: To set values of brand and year since same variable is used for instantiation this keyword used as a reference
-    public Vehicle(String brand, int year) {
-        this.brand = brand;
+
+    public Car(String brand, int year) {
+        super(brand);
         this.year = year;
     }
 
-//Abstract method: Must be implemented by all subclasses.Abstract methods don't print anything
-    abstract void drive();
-//Concrete method: Shared behaviour for all vehicles
+    @Override
+    public void startEngine() {
+        o.println("Car engine started.");
+    }
+
+    @Override
+    public void stopEngine() {
+        o.println("Car engine stopped.");
+    }
+
+    @Override
+    void drive() {
+        o.println("Driving a car.");
+    }
+
+    @Override
     public void displayInfo() {
-        Engine.o.println("Brand: " + brand + ", Year: " + year);
+        o.println("Car Brand: " + info + ", Year: " + year);
     }
 }
 
-//Subclass: Car is inherited from Vehicle that implement the interface Engine
-class Car extends Vehicle implements Engine
-{
-    //Constructor: Calls superclass constructor that sets brand and year
-    public Car(String brand, int year) {
-        super(brand, year);
-    }
+class Bike extends Vehicle implements Engine {
+    int year;
 
-    //Override: instructs the compiler that you intend to override a method in the superclass
-    @Override
-    // startEngine: implements startEngine method from Engine interface
-    public void startEngine() {
-        o.println("Car Engine is starting...");
-    }
-
-    @Override
-    //endEngine: implements endEngine method from Engine interface
-    public void endEngine() {
-        o.println("Car Engine is ending...");
-    }
-
-    @Override
-    //Drive: Implements abstract drive method from Vehicle class
-    public void drive() {
-        o.println("Car is being driving...");
-    }
-}
-
-//Subclass: Bike is inherited from Vehicle that implement the interface Engine
-class Bike extends Vehicle implements Engine
-{
-    //Constructor: Calls superclass constructor that sets brand and year
     public Bike(String brand, int year) {
-
-        super(brand, year);
+        super(brand);
+        this.year = year;
     }
 
     @Override
-    // startEngine: implements startEngine method from Engine interface
     public void startEngine() {
-        o.println("Bike Engine is starting...");
+        o.println("Bike engine started.");
     }
+
     @Override
-    // endEngine: implements endEngine method from Engine interface
-    public void endEngine() {
-        o.println("Bike Engine is ending...");
+    public void stopEngine() {
+        o.println("Bike engine stopped.");
     }
+
     @Override
-    //Drive: Implements abstract drive method from Vehicle class
-    public void drive() {
-        o.println("Bike is being driving...");
+    void drive() {
+        o.println("Riding a bike.");
+    }
+
+    @Override
+    public void displayInfo() {
+        o.println("Bike Brand: " + info + ", Year: " + year);
     }
 }
 
-////Subclass: Boat is inherited from Vehicle that implement the interface Engine
-class Boat extends Vehicle implements Engine
-{
-    //Constructor: Calls superclass constructor that sets brand and year
-    public Boat(String brand, int year) {
+class Boat extends Vehicle implements Engine {
+    int capacity;
 
-        super(brand, year);
+    public Boat(String type, int capacity) {
+        super(type);
+        this.capacity = capacity;
     }
 
     @Override
-    // startEngine: implements startEngine method from Engine interface
     public void startEngine() {
-        o.println("Boat Engine is starting...");
+        o.println("Boat engine started.");
     }
 
     @Override
-    // endEngine: implements endEngine method from Engine interface
-    public void endEngine() {
-        o.println("Boat Engine is ending...");
+    public void stopEngine() {
+        o.println("Boat engine stopped.");
     }
 
     @Override
-    //Drive: Implements abstract drive method from Vehicle class
-    public void drive() {
-        o.println("Boat is being sailing...");
+    void drive() {
+        o.println("Sailing a boat.");
+    }
+
+    @Override
+    public void displayInfo() {
+        o.println("Boat Type: " + info + ", Capacity: " + capacity + " persons");
     }
 }
 
-public class Abstraction
-{
+public class TestVehicle {
     public static void main(String[] args) {
+        PrintStream o = System.out;
 
-        Engine.o.println("I am Md.Atif Rahman Rudro.My ID is IT:22002");
-        Engine.o.println();
+        Vehicle car = new Car("Toyota", 2022);
+        car.displayInfo();
+        ((Engine) car).startEngine();
+        car.drive();
+        ((Engine) car).stopEngine();
 
-        // Create a Car object using a Vehicle reference (polymorphism)
-        Vehicle car = new Car("X-Corolla", 2002);
-        car.displayInfo(); // Calls concrete method from Vehicle
-        ((Engine) car).startEngine(); // Cast to Engine to access startEngine
-        car.drive(); // Calls overridden drive method in Car
-        ((Engine) car).endEngine(); // Cast to Engine to access stopEngine
-        Engine.o.println(); //Just a line break
+        o.println();
 
-        // Create a Bike object using a Vehicle reference (polymorphism)
-        Vehicle bike = new Bike("Hero-Honda", 2001);
-        bike.displayInfo(); // Calls concrete method from Vehicle
-        ((Engine) bike).startEngine(); // Cast to Engine to access startEngine
-        bike.drive(); // Calls overridden drive method in bike
-        ((Engine) bike).endEngine(); // Cast to Engine to access stopEngine
-        Engine.o.println(); //Just a line break
+        Vehicle bike = new Bike("Yamaha", 2023);
+        bike.displayInfo();
+        ((Engine) bike).startEngine();
+        bike.drive();
+        ((Engine) bike).stopEngine();
 
-        // Create a Boat object using a Vehicle reference (polymorphism)
-        Vehicle boat = new Boat("Osprey", 2022);
-        boat.displayInfo(); //Calls concrete method from Vehicle
-        ((Engine) boat).startEngine(); // Cast to Engine to access startEngine
-        boat.drive(); // Calls overridden drive method in Boat
-        ((Engine) boat).endEngine(); // Cast to Engine to access stopEngine
+        o.println();
+
+        Vehicle boat = new Boat("Fishing", 8);
+        boat.displayInfo();
+        ((Engine) boat).startEngine();
+        boat.drive();
+        ((Engine) boat).stopEngine();
     }
 }
